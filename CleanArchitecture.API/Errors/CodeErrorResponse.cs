67efbc00b9c29662b6887b6122be6bc6a125fd08
Fabian Monzon <fabianalexis06@gmail.com>
2022-02-15 15:50:51 -0300

    Add Middleware

---
+++ CleanArchitecture.API/Errors/CodeErrorResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace CleanArchitecture.API.Errors
+{
+    public class CodeErrorResponse
+    {
+        public CodeErrorResponse(int statusCode, string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; private set; }
+        public string? Message { get; private set; }
+
+        private string GetDefaultMessageStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "El request enviado tiene errores",
+                401 => "No tienes autorizacion para este recurso",
+                404 => "No se encontro el recurso solicitado",
+                500 => "Se produjeron errores en el servidor",
+                _   => string.Empty
+            };
+        }
+    }
+}
---
+++ CleanArchitecture.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using CleanArchitecture.API.Errors;
+using CleanArchitecture.Application.Exceptions;
+using System.Net;
+
+namespace CleanArchitecture.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            this.next = next;
+            this.logger = logger;
+            this.env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+
+                context.Response.ContentType = "application/json";
+
+                var statusCode = (int)HttpStatusCode.InternalServerError;
+                var result = string.Empty;
+
+                switch (ex)
+                {
+                    case NotFoundException notFoundException:
+                        statusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    case ValidationException validationException:
+                        statusCode = (int)HttpStatusCode.BadRequest;
+                        var validationJson = Newtonsoft.Json.JsonConvert.SerializeObject(validationException.Errors);
+                        result = Newtonsoft.Json.JsonConvert.SerializeObject(new CodeErrorException(statusCode, ex.Message, validationJson));
+                        break;
+                    case BadRequestException badRequestException:
+                        statusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    default:
+                        break;
+                }
+
+                if(string.IsNullOrEmpty(result))
+                {
+                    result = Newtonsoft.Json.JsonConvert.SerializeObject(new CodeErrorException(statusCode, ex.Message, ex.StackTrace));
+                }
+                context.Response.StatusCode = statusCode;
+
+                await context.Response.WriteAsync(result);
+            }
+        }
+    }
+}
